2689ec68705241653d9af477343015051d340c70
lizhaojin <lizhaojinzz@163.com>
2021-01-18 21:05:58 +0800

    +view&tests

--- meadowlark.js
+++ meadowlark.js
@@ -8,14 +8,31 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
-//设置handlebars视图引擎
+//测试用中间件设置，必须出现在所有路由之前
+app.use((req, res, next) => {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.text === '1';
+  next();
+})
+
+
+app.get('/tours/hood-river', (req, res) => {
+  res.render('tours/hood-river');
+})
+
+app.get('/tours/request-group-rate', (req, res) => {
+  res.render('tours/request-group-rate');
+})
+
 
 app.get('/', (req, res) => {
   res.render('home');
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {fortune: fortune.getFortune()});
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
 })
 
 app.use((req, res) => {
