f272464f32c420bb3331695cb1ad1c645974ff17
lizhaojin <lizhaojinzz@163.com>
2021-01-21 18:47:58 +0800

    form

--- meadowlark.js
+++ meadowlark.js
@@ -7,11 +7,37 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
 
 //测试用中间件设置，必须出现在所有路由之前
 app.use((req, res, next) => {
   res.locals.showTests = app.get('env') !== 'production' && req.query.text === '1';
   next();
+})
+
+//关于表单
+app.get('/headers', (req, res) => {
+  res.set('content-type', 'text/plain');
+  var s = '';
+  for (var name in req.headers) {
+    s += name + ' ' + req.headers[name] + '\n';
+  }
+  res.send(s);
+})
+
+app.get('/newsletter', (req, res) => {
+  // CSRF 提供一些虚拟值
+  res.render('newsletter', {csrf: 'CSRF token goes here'});
+})
+
+app.post('/process', (req, res) => {
+  if(req.xhr || req.accepts('json,html') === 'json') {
+    // 如果发生错误，发送error
+    res.send({success: true});
+  } else {
+    // 如果发生错误，应该重定向到错误页面
+    res.redirect(303, '/thank-you');
+  }
 })
 
 
